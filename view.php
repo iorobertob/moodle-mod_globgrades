c1e2812cc8696695fc43ff746e6a60dff6672c20
Roberto <iorobertob@gmail.com>
2021-07-05 01:05:36 +0300

    test retreiving grades

--- view.php
+++ view.php
@@ -88,13 +88,13 @@ echo $OUTPUT->header();
 
 
 if ($moduleinstance->inputdisplay === "1"){
-    echo (globgrades_build_input_table($fileurl, $course, $moduleinstance->separationchar, $moduleinstance->name));
+    echo (globgrades_build_input_table("", $course, $moduleinstance->separationchar, $moduleinstance->name));
 }
 else{
-    $grades = $DB->get_records("globgradesgrades", null, '', "student_name,course_name,grade,gradedate,teacher_name");
+    $grades = $DB->get_records("globgradesgrades", null, '', "id,student_name,course_name,grade,gradedate,teacher_name");
     var_dump($grades[1]);
     
-    echo (globgrades_build_input_table($fileurl, $course, $moduleinstance->separationchar, $moduleinstance->name));
+    echo (globgrades_build_input_table("", $course, $moduleinstance->separationchar, $moduleinstance->name));
     // echo (globgrades_build_grades_table($fileurl, $course, $moduleinstance->separationchar, $moduleinstance->name));git
 }
 
