524b60bf5bbde3365d92cf8f78af781b63c9ad51
Roberto <iorobertob@gmail.com>
2021-07-05 01:27:29 +0300

    first attempt at creating table blet

--- view.php
+++ view.php
@@ -91,7 +91,7 @@ if ($moduleinstance->inputdisplay === "1"){
     echo (globgrades_build_input_table("", $course, "separator", $moduleinstance->name));
 }
 else{
-    $grades = $DB->get_records("globgradesgrades", null, '', "student_name,course_name,grade,gradedate,teacher_name");
+    $grades = $DB->get_records("globgradesgrades", null, '', "id,student_name,course_name,grade,gradedate,teacher_name");
     
     $the_big_array = array(array( count($grades), "Course", "Grade", "Date", "Teacher"));
     foreach($grades as $one_grade){
